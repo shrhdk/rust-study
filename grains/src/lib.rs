130967f8be3aaa60f7e36609e152d736f828b8fd
Hideki SHIRO <hideki@shiro.be>
2019-07-09 00:30:03 +0900

    Simplify Grains

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -2,12 +2,12 @@ pub fn square(s: u32) -> u64 {
     if s < 1 || 64 < s {
         panic!("Square must be between 1 and 64")
     }
-    2_u64.pow(s - 1)
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
     let mut sum = 0;
-    for s in 1..65 {
+    for s in 1..=64 {
         sum += square(s);
     }
     sum
