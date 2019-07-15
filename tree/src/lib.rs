a8f3ebd79b7cfe0084ddc7be8b8ea19d59c7ae52
Hideki SHIRO <hideki@shiro.be>
2019-07-15 22:50:50 +0900

    Red-Black-Tree supports non-copy type value.

--- tree/src/lib.rs
+++ tree/src/lib.rs
@@ -1,5 +1,8 @@
 use std::cmp::Ordering::*;
 
+use Color::*;
+use Direction::*;
+
 // BTreeSet
 
 pub struct BTreeSet<T> {
@@ -114,31 +117,6 @@ enum Direction {
     Right,
 }
 
-#[derive(Eq, PartialEq, Copy, Clone)]
-enum Pattern {
-    LL,
-    RR,
-    LR,
-    RL,
-    Ok,
-}
-
-fn insert<T: Ord>(opt_node: &mut Option<Box<RBTreeNode<T>>>, new_node: RBTreeNode<T>) -> bool {
-    match opt_node {
-        Some(node) => {
-            match new_node.value.cmp(&node.value) {
-                Less => insert(&mut node.left, new_node),
-                Greater => insert(&mut node.right, new_node),
-                Equal => false,
-            }
-        }
-        None => {
-            std::mem::replace(opt_node, Some(Box::new(new_node)));
-            true
-        }
-    }
-}
-
 fn rotate_right<T>(opt_node: &mut Option<Box<RBTreeNode<T>>>) {
     let mut a = opt_node.take().unwrap();
     let mut b = a.left.take().unwrap();
@@ -155,103 +133,61 @@ fn rotate_left<T>(opt_node: &mut Option<Box<RBTreeNode<T>>>) {
     std::mem::replace(opt_node, Some(b));
 }
 
-fn rotate_left_right<T>(opt_node: &mut Option<Box<RBTreeNode<T>>>) {
-    rotate_left(&mut opt_node.as_mut().unwrap().left);
-    rotate_right(opt_node);
-}
+fn insert<T: Ord>(opt_node: &mut Option<Box<RBTreeNode<T>>>, dir: Option<Direction>, new_node: RBTreeNode<T>) -> (Option<Direction>, Option<Direction>) {
+    use Direction::*;
 
-fn rotate_right_left<T>(opt_node: &mut Option<Box<RBTreeNode<T>>>) {
-    rotate_right(&mut opt_node.as_mut().unwrap().right);
-    rotate_left(opt_node);
-}
-
-fn pattern<T: Ord>(opt_node: &Option<Box<RBTreeNode<T>>>, value: &T) -> Option<Pattern> {
-    let a = opt_node.as_ref()?;
-    let (b, dir1) = match value.cmp(&a.value) {
-        Less => (a.left.as_ref()?, Direction::Left),
-        Greater => (a.right.as_ref()?, Direction::Right),
-        Equal => return None,
-    };
-    let (c, dir2) = match value.cmp(&b.value) {
-        Less => (b.left.as_ref()?, Direction::Left),
-        Greater => (b.right.as_ref()?, Direction::Right),
-        Equal => return None,
+    let pattern = match opt_node {
+        Some(node) => {
+            match new_node.value.cmp(&node.value) {
+                Less => insert(&mut node.left, Some(Left), new_node),
+                Greater => insert(&mut node.right, Some(Right), new_node),
+                Equal => return (dir, None),
+            }
+        }
+        None => {
+            std::mem::replace(opt_node, Some(Box::new(new_node)));
+            return (dir, None);
+        }
     };
 
-    if (a.color, b.color, c.color) == (Color::Black, Color::Red, Color::Red) {
-        match (dir1, dir2) {
-            (Direction::Left, Direction::Left) => Some(Pattern::LL),
-            (Direction::Right, Direction::Right) => Some(Pattern::RR),
-            (Direction::Left, Direction::Right) => Some(Pattern::LR),
-            (Direction::Right, Direction::Left) => Some(Pattern::RL),
-        }
-    } else {
-        Some(Pattern::Ok)
-    }
-}
-
-fn balance<T: Ord>(opt_node: &mut Option<Box<RBTreeNode<T>>>, value: &T) -> bool {
-    match pattern(opt_node, value) {
-        Some(Pattern::LL) => {
-            rotate_right(opt_node);
-            opt_node.as_mut().unwrap().color = Color::Red;
-            opt_node.as_mut().unwrap().left.as_mut().unwrap().color = Color::Black;
-            opt_node.as_mut().unwrap().right.as_mut().unwrap().color = Color::Black;
-            true
-        }
-        Some(Pattern::RR) => {
-            rotate_left(opt_node);
-            opt_node.as_mut().unwrap().color = Color::Red;
-            opt_node.as_mut().unwrap().left.as_mut().unwrap().color = Color::Black;
-            opt_node.as_mut().unwrap().right.as_mut().unwrap().color = Color::Black;
-            true
-        }
-        Some(Pattern::LR) => {
+    match pattern {
+        (Some(Left), Some(Left)) => rotate_right(opt_node),
+        (Some(Right), Some(Right)) => rotate_left(opt_node),
+        (Some(Left), Some(Right)) => {
             rotate_left(&mut opt_node.as_mut().unwrap().left);
             rotate_right(opt_node);
-            opt_node.as_mut().unwrap().color = Color::Red;
-            opt_node.as_mut().unwrap().left.as_mut().unwrap().color = Color::Black;
-            opt_node.as_mut().unwrap().right.as_mut().unwrap().color = Color::Black;
-            true
         }
-        Some(Pattern::RL) => {
+        (Some(Right), Some(Left)) => {
             rotate_right(&mut opt_node.as_mut().unwrap().right);
             rotate_left(opt_node);
-            opt_node.as_mut().unwrap().color = Color::Red;
-            opt_node.as_mut().unwrap().left.as_mut().unwrap().color = Color::Black;
-            opt_node.as_mut().unwrap().right.as_mut().unwrap().color = Color::Black;
-            true
         }
-        Some(Pattern::Ok) => {
-            let next_opt_node = match value.cmp(&opt_node.as_ref().unwrap().value) {
-                Less => &mut opt_node.as_mut().unwrap().left,
-                Greater => &mut opt_node.as_mut().unwrap().right,
-                Equal => panic!(""),
-            };
-            balance(next_opt_node, value)
-        }
-        None => false,
+        _ => return (dir, pattern.0)
     }
+
+    opt_node.as_mut().unwrap().color = Red;
+    opt_node.as_mut().unwrap().left.as_mut().unwrap().color = Black;
+    opt_node.as_mut().unwrap().right.as_mut().unwrap().color = Black;
+
+    (dir, None)
 }
 
 fn check_for_debug<T>(set: &RBTreeSet<T>) {
     fn check<T>(opt_node: &Option<Box<RBTreeNode<T>>>, parent_color: Option<Color>) {
         if let Some(node) = opt_node {
-            if node.color == Color::Red {
-                if parent_color.is_none() {
-                    panic!("root is red.");
-                } else if parent_color == Some(Color::Red) {
-                    panic!("parent and child are red.");
+            match (parent_color, node.color) {
+                (None, Red) => panic!("root is red."),
+                (Some(Red), Red) => panic!("parent and child are red."),
+                _ => {
+                    check(&node.left, Some(node.color));
+                    check(&node.right, Some(node.color));
                 }
-                check(&node.left, Some(node.color));
-                check(&node.right, Some(node.color));
             }
         }
     }
     check(&set.root, None);
 }
 
-impl<T: Ord + Copy + Clone> RBTreeSet<T> {
+impl<T: Ord> RBTreeSet<T> {
     pub fn new() -> RBTreeSet<T> {
         RBTreeSet { root: None, len: 0 }
     }
@@ -281,7 +217,11 @@ impl<T: Ord + Copy + Clone> RBTreeSet<T> {
     }
 
     pub fn insert(&mut self, value: T) -> bool {
-        let color = if self.len == 0 { Color::Black } else { Color::Red };
+        if self.contains(&value) {
+            return false;
+        }
+
+        let color = if self.len == 0 { Black } else { Red };
         let new_node = RBTreeNode {
             color,
             value,
@@ -289,15 +229,11 @@ impl<T: Ord + Copy + Clone> RBTreeSet<T> {
             right: None,
         };
 
-        let inserted = insert(&mut self.root, new_node);
+        insert(&mut self.root, None, new_node);
+        self.root.as_mut().unwrap().color = Black;
+        self.len += 1;
 
-        if inserted {
-            self.len += 1;
-            while balance(&mut self.root, &value) {};
-            self.root.as_mut().unwrap().color = Color::Black;
-        }
-
-        inserted
+        true
     }
 
     pub fn clear(&mut self) {
@@ -313,8 +249,8 @@ impl<T: ToString> RBTreeSet<T> {
             let node_str = match opt_node {
                 Some(node) => {
                     let color_str = match node.color {
-                        Color::Red => "R_".to_string(),
-                        Color::Black => "B_".to_string(),
+                        Red => "R_".to_string(),
+                        Black => "B_".to_string(),
                     };
                     color_str + &node.value.to_string()
                 }
