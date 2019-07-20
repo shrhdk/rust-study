b801811f1f2b6c1137cf0c1dfaf11552c83efe06
Hideki SHIRO <hideki@shiro.be>
2019-07-20 23:28:26 +0900

    Simplify Anagram.

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,19 +1,16 @@
 use std::collections::HashSet;
 
-fn normalize(word: &str) -> Vec<char> {
-    let mut chars: Vec<char> = word.to_lowercase().chars().collect();
+fn norm(word: &str) -> Vec<char> {
+    let mut chars: Vec<_> = word.to_lowercase().chars().collect();
     chars.sort_by(Ord::cmp);
     chars
 }
 
 pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let w = normalize(word);
-    let mut result: HashSet<&'a str> = HashSet::new();
-    for candidate in possible_anagrams {
-        let c = normalize(candidate);
-        if c == w && word.to_lowercase() != candidate.to_lowercase() {
-            result.insert(*candidate);
-        }
-    }
-    result
+    let norm_word = norm(word);
+    possible_anagrams.iter()
+        .filter(|candidate| word.to_lowercase() != candidate.to_lowercase())
+        .filter(|candidate| norm(candidate) == norm_word)
+        .cloned()
+        .collect()
 }
