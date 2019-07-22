507a90cfd59230cd7cf4715d629d36d00e07abb1
Hideki <hideki@shiro.be>
2019-07-22 13:12:23 +0900

    Simplify Anagram.

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -2,15 +2,15 @@ use std::collections::HashSet;
 
 fn norm(word: &str) -> Vec<char> {
     let mut chars: Vec<_> = word.to_lowercase().chars().collect();
-    chars.sort_by(Ord::cmp);
+    chars.sort();
     chars
 }
 
-pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let norm_word = norm(word);
     possible_anagrams
         .iter()
-        .filter(|candidate| word.to_lowercase() != candidate.to_lowercase())
+        .filter(|candidate| candidate.to_lowercase() != word.to_lowercase())
         .filter(|candidate| norm(candidate) == norm_word)
         .cloned()
         .collect()
