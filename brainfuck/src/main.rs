e669b3e605e686b4d0e957849781d3648da88bba
Hideki SHIRO <hideki@shiro.be>
2019-09-10 22:35:36 +0900

    use std::fs::read

--- brainfuck/src/main.rs
+++ brainfuck/src/main.rs
@@ -1,5 +1,3 @@
-use std::io::Read;
-
 use brainfuck::Interpreter;
 
 fn main() -> Result<(), String> {
@@ -7,16 +5,7 @@ fn main() -> Result<(), String> {
     if args.len() < 2 {
         return Err(format!("Usage: {} hoge.bf", &args[0]));
     }
-    let file_name = &args[1];
-    let program = read_all(file_name).map_err(|err| err.to_string())?;
-    let mut interpreter = Interpreter::new();
-    interpreter.interpret(&program, &mut std::io::stdin(), &mut std::io::stdout())?;
+    let program = std::fs::read(&args[1]).map_err(|err| err.to_string())?;
+    Interpreter::new().interpret(&program, &mut std::io::stdin(), &mut std::io::stdout())?;
     Ok(())
-}
-
-fn read_all(file_name: &str) -> std::io::Result<Vec<u8>> {
-    let mut file = std::fs::File::open(file_name)?;
-    let mut buf = Vec::new();
-    let _ = file.read_to_end(&mut buf)?;
-    Ok(buf)
 }
