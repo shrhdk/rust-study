64116803e13af649aa605e4899f4f1af80eefd25
Hideki SHIRO <hideki@shiro.be>
2019-09-21 16:45:31 +0900

    update wc

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::{stdin, BufRead, BufReader, Read, Result};
+use std::io::{BufRead, BufReader, Read, Result, stdin};
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
@@ -21,27 +22,22 @@ fn count_and_print<R: Read>(reader: &mut R) {
     }
 }
 
-fn count_files_and_print<S: AsRef<str>>(file_names: &[S]) {
+fn count_files_and_print<P: AsRef<Path>>(paths: &[P]) {
     let mut total_lines: usize = 0;
     let mut total_words: usize = 0;
     let mut total_bytes: usize = 0;
 
-    for file_name in file_names {
-        let file_name = file_name.as_ref();
-        match File::open(file_name) {
-            Ok(file) => match count(&file) {
-                Ok((lines, words, bytes)) => {
-                    total_lines += lines;
-                    total_words += words;
-                    total_bytes += bytes;
-                    println!("{: >8}{: >8} {: >8} {}", lines, words, bytes, file_name);
-                }
-                Err(e) => {
-                    eprintln!("{}", e);
-                }
-            },
+    for path in paths {
+        let path = path.as_ref();
+        match count_file(&path) {
+            Ok((lines, words, bytes)) => {
+                total_lines += lines;
+                total_words += words;
+                total_bytes += bytes;
+                println!("{: >8}{: >8} {: >8} {}", lines, words, bytes, path.display());
+            }
             Err(e) => {
-                eprintln!("{}: {}", file_name, e);
+                eprintln!("{}", e);
             }
         }
     }
@@ -52,33 +48,37 @@ fn count_files_and_print<S: AsRef<str>>(file_names: &[S]) {
     );
 }
 
+fn count_file<P: AsRef<Path>>(path: P) -> Result<(usize, usize, usize)> {
+    let file = File::open(path.as_ref())?;
+    count(file)
+}
+
 fn count<R: Read>(reader: R) -> Result<(usize, usize, usize)> {
     let mut lines: usize = 0;
     let mut words: usize = 0;
     let mut count_reader = CountReader::new(reader);
     let buf_reader = BufReader::new(&mut count_reader);
     for line in buf_reader.lines() {
-        let line = line?;
-        lines += 1;
-        let mut last_is_whitespace = true;
-        for c in line.chars() {
-            if !c.is_whitespace() & last_is_whitespace {
+        let mut prev_c = ' ';
+        for c in line?.chars() {
+            if !c.is_whitespace() & prev_c.is_whitespace() {
                 words += 1;
             }
-            last_is_whitespace = c.is_whitespace()
+            prev_c = c
         }
+        lines += 1;
     }
-    Ok((lines, words, count_reader.n))
+    Ok((lines, words, count_reader.bytes))
 }
 
 struct CountReader<R: Read> {
     inner: R,
-    n: usize,
+    bytes: usize,
 }
 
 impl<R: Read> CountReader<R> {
     fn new(inner: R) -> Self {
-        CountReader { inner, n: 0 }
+        CountReader { inner, bytes: 0 }
     }
 }
 
@@ -86,7 +86,7 @@ impl<R: Read> Read for CountReader<R> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         let result = self.inner.read(buf);
         if let Ok(n) = result {
-            self.n += n;
+            self.bytes += n;
         }
         result
     }
@@ -94,44 +94,9 @@ impl<R: Read> Read for CountReader<R> {
 
 #[test]
 fn test_count() {
-    assert_eq!(
-        count("hello world".as_bytes()).unwrap(),
-        Length {
-            lines: 1,
-            words: 2,
-            bytes: 11,
-        }
-    );
-    assert_eq!(
-        count("a b c".as_bytes()).unwrap(),
-        Length {
-            lines: 1,
-            words: 3,
-            bytes: 5,
-        }
-    );
-    assert_eq!(
-        count(" a b c ".as_bytes()).unwrap(),
-        Length {
-            lines: 1,
-            words: 3,
-            bytes: 7,
-        }
-    );
-    assert_eq!(
-        count("a\nb\nc".as_bytes()).unwrap(),
-        Length {
-            lines: 3,
-            words: 3,
-            bytes: 5,
-        }
-    );
-    assert_eq!(
-        count("\na\nb\nc\n".as_bytes()).unwrap(),
-        Length {
-            lines: 4,
-            words: 3,
-            bytes: 7,
-        }
-    );
+    assert_eq!(count("hello world".as_bytes()).unwrap(), (1, 2, 11));
+    assert_eq!(count("a b c".as_bytes()).unwrap(), (1, 3, 5));
+    assert_eq!(count(" a b c ".as_bytes()).unwrap(), (1, 3, 7));
+    assert_eq!(count("a\nb\nc".as_bytes()).unwrap(), (3, 3, 5));
+    assert_eq!(count("\na\nb\nc\n".as_bytes()).unwrap(), (4, 3, 7));
 }
