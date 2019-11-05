3f5e2fe8507bdecf9b6ddd26ed2a502f7c8e763c
Hideki SHIRO <hideki@shiro.be>
2019-11-05 15:03:46 +0900

    json::parse supports object, array, number, string.

--- json/src/lib.rs
+++ json/src/lib.rs
@@ -1,8 +1,9 @@
 extern crate utf8reader;
 
 use std::collections::HashMap;
-use std::io::{Error, ErrorKind, Read};
 use std::io::Result;
+use std::io::{Error, ErrorKind, Read};
+use std::str::FromStr;
 
 use utf8reader::UTF8Reader;
 
@@ -22,7 +23,10 @@ pub fn parse<R: Read>(reader: R) -> Result<Json> {
     let value = json_reader.read_value()?;
     json_reader.skip_white_spaces()?;
     if let Some(ch) = json_reader.read_char()? {
-        Err(Error::new(ErrorKind::InvalidInput, format!("unexpected char '{}', want EOF", ch)))
+        Err(Error::new(
+            ErrorKind::InvalidInput,
+            format!("unexpected char '{}', want EOF", ch),
+        ))
     } else {
         Ok(value)
     }
@@ -40,51 +44,171 @@ struct JsonReader<R: Read> {
 impl<R: Read> JsonReader<R> {
     fn new(inner: R) -> JsonReader<R> {
         let inner = UTF8Reader::new(inner);
-        JsonReader { inner, peeked_ch: None }
+        JsonReader {
+            inner,
+            peeked_ch: None,
+        }
     }
 
     fn read_value(&mut self) -> Result<Json> {
         match self.peek_char()? {
-            Some(ch) => {
-                match ch {
-                    '"' => self.read_string(),
-                    '-' | _ if ch.is_ascii_digit() => self.read_number(),
-                    '{' => self.read_object(),
-                    '[' => self.read_array(),
-                    't' => self.read_true(),
-                    'f' => self.read_false(),
-                    'n' => self.read_null(),
-                    _ => Err(Error::new(ErrorKind::InvalidInput, format!("unexpected char {}", ch)))
-                }
-            }
-            None => Err(Error::new(ErrorKind::InvalidInput, "unexpected EOF"))
+            Some(ch) => match ch {
+                '"' => self.read_string(),
+                '-' => self.read_number(),
+                ch if ch.is_ascii_digit() => self.read_number(),
+                '{' => self.read_object(),
+                '[' => self.read_array(),
+                't' => self.read_true(),
+                'f' => self.read_false(),
+                'n' => self.read_null(),
+                _ => Err(Error::new(
+                    ErrorKind::InvalidInput,
+                    format!("unexpected char {}", ch),
+                )),
+            },
+            None => Err(Error::new(ErrorKind::InvalidInput, "unexpected EOF")),
         }
     }
 
     fn read_string(&mut self) -> Result<Json> {
-        // TODO(shiro): Support escaped char.
+        let mut s = String::new();
         self.skip("\"")?;
-        let mut v = Vec::<char>::new();
         while let Some(ch) = self.read_char()? {
-            if ch == '"' {
-                break;
-            } else {
-                v.push(ch);
+            match ch {
+                '"' => return Ok(Json::String(s)),
+                '\\' => s.push(self.read_escaped_char()?),
+                _ => s.push(ch),
             }
         }
-        Ok(Json::String(v.iter().collect()))
+        Err(Error::new(ErrorKind::InvalidInput, "unexpected EOF"))
     }
 
     fn read_number(&mut self) -> Result<Json> {
-        unimplemented!()
+        let mut s = String::new();
+
+        // read negative sign or first char of mantissa
+        match self.read_char()? {
+            Some('-') => s.push('-'),
+            Some(ch) if ch.is_ascii_digit() => s.push(ch),
+            Some(ch) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidInput,
+                    format!("unexpected char '{}', want '-' or digit", ch),
+                ));
+            }
+            None => return Err(Error::new(ErrorKind::InvalidInput, "unexpected EOF")),
+        }
+
+        // read mantissa
+        loop {
+            match self.peek_char()? {
+                Some(ch) if ch.is_ascii_digit() => {
+                    self.read_char()?;
+                    s.push(ch)
+                }
+                Some('e') | Some('E') => {
+                    self.read_char()?;
+                    s.push('e');
+                    break;
+                }
+                _ => match f64::from_str(&s) {
+                    Ok(number) => return Ok(Json::Number(number)),
+                    Err(_) => {
+                        return Err(Error::new(
+                            ErrorKind::InvalidInput,
+                            format!("invalid format number {}", &s),
+                        ));
+                    }
+                },
+            }
+        }
+
+        // read exponent sign
+        let exponent_sign = self.read_char()?;
+        match exponent_sign {
+            Some('-') | Some('+') => s.push(exponent_sign.unwrap()),
+            Some(ch) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidInput,
+                    format!("unexpected char '{}', want '-' or '+'", ch),
+                ));
+            }
+            None => return Err(Error::new(ErrorKind::InvalidInput, "unexpected EOF")),
+        }
+
+        // read exponent digits
+        loop {
+            match self.peek_char()? {
+                Some(ch) if ch.is_ascii_digit() => {
+                    self.read_char()?;
+                    s.push(ch);
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+
+        match f64::from_str(&s) {
+            Ok(number) => Ok(Json::Number(number)),
+            Err(_) => Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!("invalid format number {}", &s),
+            )),
+        }
     }
 
     fn read_object(&mut self) -> Result<Json> {
-        unimplemented!()
+        let mut obj = HashMap::<String, Json>::new();
+        self.skip("{")?;
+        loop {
+            self.skip_white_spaces()?;
+            let key = if let Json::String(key) = self.read_string()? {
+                key
+            } else {
+                unreachable!()
+            };
+            self.skip_white_spaces()?;
+            self.skip(":")?;
+            self.skip_white_spaces()?;
+            let value = self.read_value()?;
+            self.skip_white_spaces()?;
+            obj.insert(key, value);
+            match self.read_char()? {
+                Some(',') => continue,
+                Some('}') => break,
+                Some(ch) => {
+                    return Err(Error::new(
+                        ErrorKind::InvalidInput,
+                        format!("unexpected char '{}'", ch),
+                    ));
+                }
+                None => return Err(Error::new(ErrorKind::InvalidInput, "unexpected EOF")),
+            }
+        }
+        Ok(Json::Object(Box::new(obj)))
     }
 
     fn read_array(&mut self) -> Result<Json> {
-        unimplemented!()
+        let mut array = Vec::<Json>::new();
+        self.skip("[")?;
+        loop {
+            self.skip_white_spaces()?;
+            array.push(self.read_value()?);
+            self.skip_white_spaces()?;
+            match self.read_char()? {
+                Some(',') => continue,
+                Some(']') => break,
+                Some(ch) => {
+                    return Err(Error::new(
+                        ErrorKind::InvalidInput,
+                        format!("unexpected char '{}'", ch),
+                    ));
+                }
+                None => return Err(Error::new(ErrorKind::InvalidInput, "unexpected EOF")),
+            }
+        }
+        Ok(Json::Array(array))
     }
 
     fn read_true(&mut self) -> Result<Json> {
@@ -102,8 +226,50 @@ impl<R: Read> JsonReader<R> {
         Ok(Json::Null)
     }
 
-    fn read_escaped_char() -> Result<char> {
-        unimplemented!()
+    fn read_escaped_char(&mut self) -> Result<char> {
+        match self.read_char()? {
+            Some('"') => Ok('"'),
+            Some('\\') => Ok('\\'),
+            Some('/') => Ok('/'),
+            Some('b') => Ok('\x08'),
+            Some('f') => Ok('\x0C'),
+            Some('n') => Ok('\x0A'),
+            Some('r') => Ok('\x0D'),
+            Some('t') => Ok('\x09'),
+            Some('u') => self.read_codepoint(),
+            Some(ch) => Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!("unexpected char {}", ch),
+            )),
+            None => Err(Error::new(ErrorKind::InvalidInput, "unexpected EOF")),
+        }
+    }
+
+    fn read_codepoint(&mut self) -> Result<char> {
+        // TODO(shiro): Support surrogate pair
+        let mut digits = String::new();
+        for _ in 0..4 {
+            if let Some(ch) = self.read_char()? {
+                if !ch.is_digit(16) {
+                    return Err(Error::new(
+                        ErrorKind::InvalidInput,
+                        format!("unexpected char '{}', want digit", ch),
+                    ));
+                }
+                digits.push(ch)
+            } else {
+                return Err(Error::new(ErrorKind::InvalidInput, "unexpected EOF"));
+            }
+        }
+        let codepoint = u32::from_str_radix(&digits, 16).unwrap();
+        if let Some(ch) = std::char::from_u32(codepoint) {
+            Ok(ch)
+        } else {
+            Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!("Bad codepoint {}", codepoint),
+            ))
+        }
     }
 
     fn skip_white_spaces(&mut self) -> Result<()> {
@@ -121,7 +287,10 @@ impl<R: Read> JsonReader<R> {
         for want_ch in s.chars() {
             let got_ch = self.read_char()?.unwrap();
             if got_ch != want_ch {
-                return Err(Error::new(ErrorKind::InvalidInput, format!("unexpected char '{}', want '{}'", got_ch, want_ch)));
+                return Err(Error::new(
+                    ErrorKind::InvalidInput,
+                    format!("unexpected char '{}', want '{}'", got_ch, want_ch),
+                ));
             }
         }
         Ok(())
--- json/tests/json.rs
+++ json/tests/json.rs
@@ -1,10 +1,126 @@
 extern crate json;
+#[macro_use]
+extern crate maplit;
 
 use json::Json;
 
 #[test]
+fn test_parse_complex_object() {
+    let obj = Json::Object(Box::new(hashmap! {
+        "array".to_string() => Json::Array(vec![
+            Json::Object(Box::new(hashmap! {
+                "num".to_string() => Json::Number(1.0),
+                "str".to_string() => Json::String("hello".to_string()),
+            })),
+            Json::Number(2.0),
+            Json::String("world".to_string()),
+        ]),
+    }));
+    assert_eq!(
+        json::parse_str(
+            r#"
+    {
+        "array": [
+            {
+                "num": 1,
+                "str": "hello"
+            },
+            2,
+            "world"
+        ]
+    }
+    "#
+        )
+        .unwrap(),
+        obj
+    );
+}
+
+#[test]
+fn test_parse_object_with_1_pair() {
+    let obj = Json::Object(Box::new(hashmap! {
+        "hello".to_string() => Json::String("world".to_string()),
+    }));
+    assert_eq!(json::parse_str(r#"{"hello":"world"}"#).unwrap(), obj);
+}
+
+#[test]
+fn test_parse_object_with_2_pairs() {
+    let obj = Json::Object(Box::new(hashmap! {
+        "hello".to_string() => Json::String("world".to_string()),
+        "foo".to_string() => Json::String("bar".to_string()),
+    }));
+    assert_eq!(
+        json::parse_str(r#"{"hello":"world","foo":"bar"}"#).unwrap(),
+        obj
+    );
+}
+
+#[test]
+fn test_parse_array_with_1_value() {
+    let array = Json::Array(vec![Json::String("hello".to_string())]);
+    assert_eq!(json::parse_str(r#"["hello"]"#).unwrap(), array);
+}
+
+#[test]
+fn test_parse_array_with_2_values() {
+    let array = Json::Array(vec![
+        Json::String("hello".to_string()),
+        Json::String("world".to_string()),
+    ]);
+    assert_eq!(json::parse_str(r#"["hello","world"]"#).unwrap(), array);
+}
+
+#[test]
 fn test_parse_string() {
-    assert_eq!(json::parse_str(r#""hello""#).unwrap(), Json::String("hello".to_string()))
+    assert_eq!(
+        json::parse_str(r#""hello""#).unwrap(),
+        Json::String("hello".to_string())
+    );
+}
+
+#[test]
+fn test_parse_string_with_lf() {
+    assert_eq!(
+        json::parse_str(r#""hello\nworld""#).unwrap(),
+        Json::String("hello\nworld".to_string())
+    );
+}
+
+#[test]
+fn test_parse_string_with_codepoint() {
+    assert_eq!(
+        json::parse_str(r#""hello \u0052\u0075\u0073\u0074""#).unwrap(),
+        Json::String("hello Rust".to_string())
+    );
+}
+
+#[test]
+fn test_parse_string_with_surrogate_pair_codepoint() {
+    assert_eq!(
+        json::parse_str(r#""hello \u0001\uF980""#).unwrap(),
+        Json::String("hello 🦀".to_string())
+    );
+}
+
+#[test]
+fn test_parse_string_with_unexpected_eof() {
+    assert!(json::parse_str(r#""hel"#).is_err());
+}
+
+#[test]
+fn test_parse_number() {
+    assert_eq!(json::parse_str("123").unwrap(), Json::Number(123f64));
+}
+
+#[test]
+fn test_parse_number_with_positive_exponent() {
+    assert_eq!(json::parse_str("123e+3").unwrap(), Json::Number(123e+3f64));
+}
+
+#[test]
+fn test_parse_number_with_negative_exponent() {
+    assert_eq!(json::parse_str("123e-3").unwrap(), Json::Number(123e-3f64));
 }
 
 #[test]
